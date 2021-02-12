1c41ddbf991bdeffcade961f3121a34cc9187467
dorang2 <65272412+dorang2@users.noreply.github.com>
2021-02-12 22:41:55 +0700

    Add files via upload

---
+++ BLConfig.py
@@ -0,0 +1,42 @@
+#•••••••••••••••••••••••••••••••••••••••••••••••
+#     Linting - Bet (Gigabet) v1.0.0 (107)
+#•••••••••••••••••••••••••••••••••••••••••••••••
+
+Basic   ={
+        "Access Token"        : "c3a75f20502bebdfad8297dc09a04834d07e4048839b59f163ee63268c6c672b32f4ce7f4767b8983f93442261c9f1afe4e6137e27b664dbe681eabb5a4b32c9-14aRomnWjV4IhyN4LmHIDPgr6jQXHEm8LWtZjETJi0dWprkpgQFevREHh4nB9mpEOT2gv00kHWtqBIKN1b1klnq%2BSSXaP3i5VBevkgC1bDuBqYKCYWTfnKx2zGF0oXeL5BooJZr%2FS4Acm1TdGQXqgw%3D%3",
+        "Username"            : "Gong_xi_",
+        "Currency"            : "btt"
+}
+
+Betting ={
+        "Base Bet"            : 0,0000001,
+        "Chance"              : 5,       # 0.01 - 98.00
+        "Linting"             : 10,       # 10/20/60/80/100/200 (sesuaikan dengan level masing-masing)
+        "Increase After Lose" : 1.08,    # Bukan persen,
+        "Stop Balance Over"   : 0,
+        "Stop Balance Under"  : 0,
+        "Stop Amount Over"    : 0
+}
+
+# Coming soon
+# Seed    ={
+#         "If Current Profit is equal to" : 378.995,
+#         "Then, Reset Seed"              : True,
+#         "Random Seed"                   : True,
+
+#         "If Random Seed is False, then" : {
+#                                           "Seed": "xxxxxxxxxxxxxxxxxxxx"
+#                                           }
+# }
+
+#•••••••••••••••••••••••••••••••••••••••••••••••
+#  BL`378_project
+#  Linting - Bet (Gigabet) v1.0.0 (107)
+#  --
+#
+#  @taraje_dah
+#  @taraje_channel
+#  #bancetz.Laut_
+#  @ballataxart  (tg)
+#  378bancetz.laut@gmail.com
+#•••••••••••••••••••••••••••••••••••••••••••••••
